1991c47c098a49c8bdd87fca96bcb76e96104ba6
lennymuldoon <lenny.muldoon@gmail.com>
2023-12-19 09:37:28 -0600

    edited add, subtract, multiply, divide

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ import math
 
 answer = 0
 
-def add(inputNum):
-    answer += inputNum
+def add(inputNum, currentNum):
+    return currentNum + inputNum
 
-def subtract(inputNum):
-    answer -= inputNum
+def subtract(inputNum, currentNum):
+    return currentNum - inputNum
 
-def multiply(inputNum):
-    answer *= inputNum
+def multiply(inputNum, currentNum):
+    return currentNum * inputNum
 
-def divide(inputNum):
-    answer /= inputNum
+def divide(inputNum, currentNum):
+    return currentNum / inputNum
+
+
+answer += add(4, answer)
+print(answer)
+
+answer /= divide(2, answer)
+print(answer)
+
+answer -= subtract(1, answer)
+print(answer)
+
+answer *= multiply(5, answer)
+print(answer)
