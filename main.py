537de06d652c1e715b72f6d723c885e096f20513
lennymuldoon <lenny.muldoon@gmail.com>
2023-12-19 09:50:53 -0600

    created add choice

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Desc: This is a basic calculator program for me to learn python syntax
 import math
 
 answer = 0
+quit = False
 
 def add(inputNum, currentNum):
     return currentNum + inputNum
@@ -20,14 +21,12 @@ def divide(inputNum, currentNum):
     return currentNum / inputNum
 
 
-answer += add(4, answer)
-print(answer)
-
-answer /= divide(2, answer)
-print(answer)
-
-answer -= subtract(1, answer)
-print(answer)
-
-answer *= multiply(5, answer)
-print(answer)
+while not quit:
+    choice = int(input("What operation would you like to do? 1 for addition 2 for subtraction 3 for multiplication 4 for division 5 for quit"))
+    if choice == 1:
+        print("Total is currently", answer)
+        inputNum = int(input("What number would you like to add to your total?"))
+        answer = add(inputNum, answer)
+        print("New total is", answer)
+    else:
+        quit = True
