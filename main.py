b0821dc606ab80a6968c9a87bfeede738409a4f2
lennymuldoon <lenny.muldoon@gmail.com>
2023-12-21 14:06:58 -0600

    created root function

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ def divide(inputNum, currentNum):
 def exponent(inputNum, currentNum):
     return currentNum ** inputNum
 
+def root(inputNum, currentNum):
+    return currentNum ** (1 / inputNum)
 
 while not quit:
-    choice = int(input("What operation would you like to do? 1 for addition 2 for subtraction 3 for multiplication 4 for division 5 for exponent any other number for quit"))
+    choice = int(input("What operation would you like to do? 1 for addition 2 for subtraction 3 for multiplication 4 for division 5 for exponent 6 for root any other number for quit"))
     if choice == 1:
         print("Total is currently", answer)
         inputNum = int(input("What number would you like to add to your total?"))
@@ -50,6 +52,11 @@ while not quit:
         print("Total is currently", answer)
         inputNum = int(input("What number would you like to use as an exponent for your total?"))
         answer = exponent(inputNum, answer)
+        print("New total is", answer)
+    elif choice == 6:
+        print("Total is currently", answer)
+        inputNum = int(input("What number would you like to use as your root for your total?"))
+        answer = root(inputNum, answer)
         print("New total is", answer)
     else:
         quit = True
